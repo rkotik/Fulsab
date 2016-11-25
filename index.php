4feede47d55467acabe7380557427a04895b89c2
Ricardo Kotk <rkotik@gmail.com>
2016-11-25 17:23:55 -0300

    subir

--- index.php
+++ index.php
@@ -1,21 +1,41 @@
-<!DOCTYPE html>
-<html lang="es">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Mi primer titulo para GitHub</title>
+<?php
 
-		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-	</head>
-	<body>
+/**
+  * Ocrend Framework - MVC Architecture for Web Applications
+  * PHP Version 7
+  * @package Ocrend Framework
+  * @version v1.1.2
+  * @author Brayan Narváez (Prinick) <prinick@ocrend.com> <youtube.com/user/prinick96>
+  * @copyright 2016 - Ocrend Software
+  * @license	http://opensource.org/licenses/MIT	MIT License
+  * @link http://framework.ocrend.com/
+*/
 
+//------------------------------------------------
 
-		<!-- jQuery -->
-		<script src="http://code.jquery.com/jquery.js"></script>
-		<!-- Bootstrap JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
- 		<script src=""></script>
-	</body>
-</html>
+# Carga del núcleo
+define('INDEX_DIR',true);
+require('core/app_core.php');
+
+//------------------------------------------------
+
+# Detección del controlador actual
+$Controller = $router->getController();
+
+//------------------------------------------------
+
+# Identificación del controlador en el sistema
+if(!is_readable('core/controllers/' . $Controller . '.php')) {
+  $Controller = 'errorController';
+}
+
+# Carga del controlador seleccionado
+require('core/controllers/' . $Controller . '.php');
+new $Controller;
+
+//------------------------------------------------
+
+# Modo debug
+!DEBUG ?: new Debug($startime);
+
+?>
